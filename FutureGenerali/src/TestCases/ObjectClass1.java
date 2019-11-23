153fcd50984b648c3421bc023994857d8b3cbb12
Jeevan <Jeevan@Jeevan-PC>
2019-11-23 23:46:00 +0530

    OC code added

---
+++ FutureGenerali/src/TestCases/ObjectClass1.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+
+
+public class ObjectClass1
+{
+
+	static int num1= 100;
+	public static int num2=200;
+	
+static public void main(String[] args) 
+{
+	System.out.println("Below are the main method outputs :- ");
+	System.out.println(num1);
+	System.out.println(num2);
+	
+	ObjectClass1 login = new ObjectClass1();
+	System.out.println("Below are the Login method outputs :- ");
+}
+}
